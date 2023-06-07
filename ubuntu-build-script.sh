1f3761d96db106b58a6b3ed28e8f13be570548ca
Shine Wunna Aung <67731642+DevShineWunnaAung@users.noreply.github.com>
2023-06-07 22:26:02 +0630

    Update ubuntu-build-script.sh

--- ubuntu-build-script.sh
+++ ubuntu-build-script.sh
@@ -27,7 +27,7 @@ function sub-build-swapfile ()
  echo ""
  echo ""
  echo "================================================================="
- echo " About to build 2GB swapfile called /swapfile"
+ echo " About to build 4GB swapfile called /swapfile"
  echo "-----------------------------------------------------------------"
  read -p "Proceed ? (Y/n)" choice
  if [ "$choice" = "n" ]
@@ -43,7 +43,7 @@ function sub-build-swapfile ()
     else 
         echo "/swapfile does not exist. Bulding /swapfile.."
         cd /
-	    sudo dd if=/dev/zero of=/swapfile bs=1M count=2048 status=progress
+	    sudo dd if=/dev/zero of=/swapfile bs=1M count=4096 status=progress
 	    sudo chmod 600 /swapfile
 	    sudo mkswap /swapfile
 	    sudo swapon /swapfile
